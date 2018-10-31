668fe314abff313819fe8ed78a0db5d274eb47e3
toon040 <toon040@hotmail.com>
2018-10-31 13:43:32 +0700

    Learning 2

--- hello/views.py
+++ hello/views.py
@@ -12,14 +12,26 @@ def hello_there(request, name):
     now = datetime.now()
     formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
+    # # Filter the name argument to letters only using regular expressions. URL arguments
+    # # can contain arbitrary text, so we restrict to safe characters only.
+    # match_object = re.match("[a-zA-Z]+", name)
 
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
+    # if match_object:
+    #     clean_name = match_object.group(0)
+    # else:
+    #     clean_name = "Friend"
 
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return HttpResponse(content)
+    # content = "Hello there, " + clean_name + "! It's " + formatted_now
+    # return HttpResponse(content)
+
+    # BAD CODE! Avoid inline HTML for security reason, plus templates automatically escape HTML content.
+    content = "<strong>Hello there, " + name + "!</strong> It's " + formatted_now
+
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'title': 'Hello, Django',
+            'content': content,
+        }
+    )
