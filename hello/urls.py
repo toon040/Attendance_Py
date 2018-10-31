ffd397b5120cd7da4a774b3e4b22fb7fd958fde7
toon040 <toon040@hotmail.com>
2018-10-31 09:13:37 +0700

    update debug

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,5 @@ from hello import views
 
 urlpatterns = [
     path("",views.home, name="home"),
-    path("hello/<name>",views.hello_there, name="hello_there"),
+    path("hello/<name>", views.hello_there, name="hello_there"),
 ]
