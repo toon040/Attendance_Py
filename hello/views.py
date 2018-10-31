028cd792c0ae73a96b7017e288ca48c4a4d1d92a
toon040 <toon040@hotmail.com>
2018-10-31 16:11:55 +0700

    create template and start stati files

--- hello/views.py
+++ hello/views.py
@@ -8,30 +8,13 @@ from datetime import datetime
 def home(request):
     return HttpResponse("Hello, Django")
 
-def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+def hello_there(request, name):   
 
-    # # Filter the name argument to letters only using regular expressions. URL arguments
-    # # can contain arbitrary text, so we restrict to safe characters only.
-    # match_object = re.match("[a-zA-Z]+", name)
-
-    # if match_object:
-    #     clean_name = match_object.group(0)
-    # else:
-    #     clean_name = "Friend"
-
-    # content = "Hello there, " + clean_name + "! It's " + formatted_now
-    # return HttpResponse(content)
-
-    # BAD CODE! Avoid inline HTML for security reason, plus templates automatically escape HTML content.
-    content = "<strong>Hello there, " + name + "!</strong> It's " + formatted_now
-
-    return render(
+       return render(
         request,
         'hello/hello_there.html',
         {
-            'title': 'Hello, Django',
-            'content': content,
+            'name': name,
+            'date': datetime.now()
         }
     )
