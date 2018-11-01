f0dd33f04fe9e4abe5d3f3ca4afb0377d1c9d252
toon040 <toon040@hotmail.com>
2018-11-01 09:03:40 +0700

    Data model but Error

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 import re
 from django.http import HttpResponse
 from datetime import datetime
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
 
 def home(request):
     return render(request, "hello/home.html")
@@ -27,3 +31,16 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+def log_message(request):
+    if request.method == "POST":
+        form = LogMessageForm(request.POST)
+
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        form = LogMessageForm()
+        return render(request, "hello/log_message.html", {"form": form}) 
